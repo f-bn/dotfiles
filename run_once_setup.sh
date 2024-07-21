b3a0fba88dd2e691d44219e8078fd485c82f013e
f-bn <contact@fbobin.me>
2024-07-21 15:09:42 +0200

    feat: remove aiven-client package

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -36,7 +36,6 @@ git clone -q https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.14.0
 
 # Install Python packages
 echo "* Install Python packages (using pipx)"
-pipx install -q git+https://github.com/aiven/aiven-client.git
 pipx install -q git+https://github.com/nathom/streamrip.git@v2.0.3
 
 # Install ohmyzsh
