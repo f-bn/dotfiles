0cb118e0b64d6f1f6e6469c4368a38c383881c20
f-bn <contact@fbobin.me>
2025-06-19 00:05:04 +0200

    fix: incorrect variable expansion for vim-plug install

--- run_once_setup_vim.sh
+++ run_once_setup_vim.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 # Install vim plugin manager (vim-plug)
 echo "* Install vim plugin manager (vim-plug)"
-curl -sfLo "${HOME/.local/share}/nvim/site/autoload/plug.vim" \
+curl -sfLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" \
   --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
