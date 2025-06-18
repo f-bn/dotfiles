2e8d1036c344939272076ea1794b5a583ca78e60
f-bn <contact@fbobin.me>
2025-06-18 22:39:00 +0200

    feat: update run_once_setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -3,10 +3,9 @@
 set -eo pipefail
 
 INSTALL_DIR="$HOME/.local/bin"
-TEMP_DIR="/tmp/chezmoi.temp"
+TEMP_DIR="$(mktemp -d)"
 
 mkdir -p "${INSTALL_DIR}"
-mkdir -p "${TEMP_DIR}"
 
 # Install Zellij
 echo "* Install Zellij"
@@ -32,23 +31,19 @@ chmod +x "${INSTALL_DIR}/incus"
 
 # Install Python packages
 echo "* Install Python packages (using pipx)"
-pipx install -q streamrip
-
-# Install ohmyzsh
-echo "* Install ohmyzsh"
-git clone -q https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
-
-# Install ohmyzsh autosuggestions
-echo "* Install ohmyzsh autosuggestions plugin"
-git clone -q https://github.com/zsh-users/zsh-autosuggestions "${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+pipx install -q --force exegol streamrip
 
 # Install vim plugin manager (vim-plug)
 echo "* Install vim plugin manager"
-curl -sfLo "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" \
+curl -sfLo "${HOME/.local/share}/nvim/site/autoload/plug.vim" \
   --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install ohmyzsh
+echo "* Install ohmyzsh"
+git clone -q https://github.com/ohmyzsh/ohmyzsh.git "${HOME}/.oh-my-zsh"
+
 # Change user default shell
-sudo chsh -s /usr/bin/zsh f-bn
+sudo chsh -s /usr/bin/zsh fbobin
 
 # Cleanup
 rm -rf "${TEMP_DIR}"
