2ba091518a7f4ffb517e082bf22462bcec9ea855
f-bn <contact@fbobin.me>
2025-06-21 12:44:30 +0200

    feat: add idempotency for tasks

--- run_once_setup_vim.sh
+++ run_once_setup_vim.sh
@@ -4,5 +4,10 @@ set -eo pipefail
 
 # Install vim plugin manager (vim-plug)
 echo "* Install vim plugin manager (vim-plug)"
-curl -sfLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" \
-  --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -f "${HOME}/.local/share/nvim/site/autoload/plug.vim" ]; then
+  mkdir -p "${HOME}/.local/share/nvim/site/autoload"
+  curl -sfLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+  echo "> Already installed, skipping"
+fi
--- run_once_setup_zsh.sh
+++ run_once_setup_zsh.sh
@@ -3,11 +3,23 @@
 set -eo pipefail
 
 OMZ_DIR="${HOME}/.oh-my-zsh"
+OMZ_PLUGIN_DIR="${OMZ_DIR}/custom/plugins"
 
 # Install ohmyzsh
 echo "* Install ohmyzsh"
-git clone -q https://github.com/ohmyzsh/ohmyzsh.git "${OMZ_DIR}"
+if [ ! -d "${OMZ_DIR}" ]; then
+  mkdir -p "${OMZ_DIR}"
+  git clone -q https://github.com/ohmyzsh/ohmyzsh.git "${OMZ_DIR}"
+else
+  echo "> Oh My Zsh already installed, skipping"
+fi
+
 
 # Install ohmyzsh autosuggestions plugin
 echo "* Install zsh autosuggestions plugin"
-git clone -q https://github.com/zsh-users/zsh-autosuggestions "${OMZ_DIR}/custom/plugins/zsh-autosuggestions"
+if [ ! -d "${OMZ_PLUGIN_DIR}/zsh-autosuggestions" ]; then
+  mkdir -p "${OMZ_PLUGIN_DIR}/custom/plugins"
+  git clone -q https://github.com/zsh-users/zsh-autosuggestions "${OMZ_PLUGIN_DIR}/zsh-autosuggestions"
+else
+  echo "> Already installed, skipping"
+fi
